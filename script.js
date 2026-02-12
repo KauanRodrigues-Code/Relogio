111e7ad3340b6e3deeacfbc86ca340185d66dac8
KauanRodrigues-Code <kauancorinthiano@icloud.com>
2026-02-12 14:38:14 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+gsap.registerPlugin(ScrollTrigger);
 
+gsap.from(".hero h1",{opacity:0,y:40,duration:1});
+gsap.from(".hero p",{opacity:0,y:40,duration:1,delay:.3});
+gsap.from(".hero img",{opacity:0,scale:.9,duration:1.2,delay:.6});
+
+gsap.utils.toArray("section").forEach(section=>{
+  gsap.from(section,{
+    scrollTrigger:{
+      trigger:section,
+      start:"top 80%"
+    },
+    opacity:0,
+    y:50,
+    duration:1
+  });
+});
